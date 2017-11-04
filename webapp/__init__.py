4cb2a560a2e48a78214bf86c75001d62af44ecb5
Roee Shenberg <shenberg@gmail.com>
2017-11-04 11:48:56 +0200

    remove run_algorithm url, merge with model choice form

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -11,22 +11,25 @@ app.config.from_object('website_config')
 def model_choice_form():
     return render_template('model_choice.html')
 
-@app.route('/choose_model')
+@app.route('/choose_model', methods=['GET', 'POST'])
 def algorithm_choice_form():
-    model_id = request.args.get('model', '')
-    if not model_id: # TODO: or not valid model
+    # take model id either from form or from get param, prefer the form
+    model_id = request.values.get('model_id','') or request.values.get('model', '')
+
+    if not model_id: # TODO: or not valid model id
         #TODO: implement flash display 
         flash('Must select valid model to choose the algorithm')
         return redirect(url_for('model_choice_form'))
 
-    return render_template('algorithm_choice.html', model_id=model_id)
+    errors = {}
+    if request.method == 'POST':
+        #TODO: validate form, run algorithm
+        # reqparse is a simple module for validation, matches up with argparse
+        algorithm_results_id = 'placeholder'
+        return redirect(url_for('show_results', result_id=algorithm_results_id))
 
-@app.route('/run_algorithm', methods=['POST'])
-def run_algorithm():
-    #TODO: validate form, run algorithm
-    # reqparse is a simple module for validation, matches up with argparse
-    algorithm_results_id = "placeholder"
-    return redirect(url_for('show_results', result_id=algorithm_results_id))
+    return render_template('algorithm_choice.html', model_id=model_id, errors=errors)
+
 
 @app.route('/explore/<result_id>')
 def show_results(result_id):
