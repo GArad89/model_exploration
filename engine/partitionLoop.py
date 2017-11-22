60708895bb793e3efd8bf3628023e2d1e6a734e4
Guy arad <garad89@gmail.com>
2017-11-22 20:57:39 +0200

    missed 1 file to the last commit

--- engine/partitionLoop.py
+++ engine/partitionLoop.py
@@ -1,8 +1,10 @@
 from graph import *
-from cluster import BranchAndBoundCluster,SpectralCluster
-from partition import SizeCriteria
+from cluster import BranchAndBoundCluster,SpectralCluster, KmeansClustering
+from stopCriteria import *
 from dendrogram import Node,Dendrogram
 
+###old code
+"""
 def is_simple(dgraph, simpletype = SizeCriteria ,threshold = 20):
       
      return simpletype(threshold).check(dgraph)
@@ -10,14 +12,13 @@ def is_simple(dgraph, simpletype = SizeCriteria ,threshold = 20):
 def cluster(dgraph,  clustertype = SpectralCluster, **params):
     
     return clustertype.cluster(dgraph,**params)
-
+"""
 
 #need to check if we want to add a stop critrea for after x runs.    
-def partition(dgraph, state_subset, clustertype = SpectralCluster, simpletype = SizeCriteria, threshold = 20, dendrogram = None, rootnode = 0):
-    print("check")
+def partition(dgraph, state_subset=None, clustering_algo = SpectralCluster, stopCri = SizeCriteria(20), dendrogram = None, rootnode = 0):
     projected_graph = dgraph.project(state_subset)
     #print(projected_graph.nodes())
-    
+    #print(projected_graph.nodes())
     if dendrogram == None:  #in case no dendrogram was initiated
         dendrogram = Dendrogram(dgraph)
     else:
@@ -26,13 +27,14 @@ def partition(dgraph, state_subset, clustertype = SpectralCluster, simpletype = 
             dendrogram.add_child(rootnode,len(dendrogram.nodes())-1)
         rootnode = len(dendrogram.nodes())-1 
         
-    if is_simple(projected_graph, simpletype, threshold):
+    if stopCri.check(projected_graph):
+        #print("check2")
         return dendrogram
 
-    clusters = cluster(projected_graph, clustertype)
+    clusters = clustering_algo.cluster(projected_graph)
+    #print("clusters: ", clusters)
     for cluster_iter in clusters:
-        #print(cluster_iter)
-        partition(dgraph, cluster_iter, clustertype, simpletype , threshold , dendrogram , rootnode )
+        partition(dgraph, cluster_iter, clustering_algo, stopCri , dendrogram , rootnode )
 
     return dendrogram
 
@@ -40,26 +42,36 @@ def partition(dgraph, state_subset, clustertype = SpectralCluster, simpletype = 
 
 def spectralcluster_test():
 
-    g = DGraph.read_dot("./dot/g2.dot")
+    g = DGraph.read_dot("./dot/weighted_g2.dot")
     print("testing SpectralCluster on g2.dot:")
-    print(cluster(g))
+    print(SpectralCluster.cluster(g))
     print("\n")
 
 def partition_test():
 
     den = None
-    g = DGraph.read_dot("./dot/g2.dot")
+    g = DGraph.read_dot("./dot/test.dot")
     #den=partition(g,g.nodes()) 
     #print(len(den.node_list))  #should be 1 (root node only)
-    print("testing partition on g2.dot:")
-    den = partition(g,g.nodes(), SpectralCluster, SizeCriteria, 2)
+    print("testing partition on g2.dot for threshold=4:")
+    den = partition(g,g.nodes(), SpectralCluster, SizeCriteria(4))
     print("the number of super-nodes in the dendogram (including the root):")
     print(len(den.node_list))  #should be 3.
     
     if(len(den.node_list)>1):
         print("the number of nodes in the 1st node (1st one after the root):")
         print(den.node_list[1].subset)
+        
+    print("testing partition on g2.dot for threshold=2:")
+    den = partition(g,g.nodes(), SpectralCluster, SizeCriteria(2),dendrogram=None)
+    print("the number of super-nodes in the dendogram (including the root):")
+    print(len(den.node_list))  #should be 7.
+    
+    if(len(den.node_list)>1):
+        print("the number of nodes in the 6th node (6th one after the root):")
+        print(den.node_list[6].subset)
+     
 
        
-#spectralcluster_test()
+spectralcluster_test()
 partition_test()
