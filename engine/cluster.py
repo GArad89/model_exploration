6b377360308b84159e877e9b71725bb9f106c537
tamir <tamirdennis@gmail.com>
2017-11-25 15:20:47 +0200

    updated code from roee and dev

--- engine/cluster.py
+++ engine/cluster.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+from graph import DGraph
+from sklearn.cluster import SpectralClustering
+import numpy as np
+import networkx as nx
 
 class Cluster(ABC):
 
@@ -9,9 +13,51 @@ class Cluster(ABC):
         """
 
 class BranchAndBoundCluster (Cluster):
+    def getParams():
+        return {},[] #TODO
+
+    
     def cluster(self, dgraph):
         pass #TODO: implement
 
 class SpectralCluster (Cluster):
-    def cluster(self, dgraph):
-        pass #TODO: implement
+
+    def getParams():
+        form = [{'key': 'n', 'type': 'text'},{'key': 'affinity', 'type': 'text'}]
+        schema = {
+            'n' : {'type': 'integer', 'title': 'number of clusters', 'minimum' : 2, 'required' : True},
+            'affinity' : {'type': 'string', 'title': 'affinity'}
+            }
+        return schema, form
+        
+
+
+
+
+        
+    def cluster(dgraph, n = 2, affinity='amg'):
+        """
+        just the basics required for the SpectralClustering algorithm for now.
+        need to test what kind of output it gives.
+        """
+        #adjacency matrix
+        adj_mat =dgraph.adjacency_matrix()
+        
+        #SpectralClustering
+        sc = SpectralClustering(n)
+        sc.fit(adj_mat)
+        result=sc.labels_
+
+        #seperating the result list to lists for each cluster (1= the node is in the substae 0= the node is not in the state)
+        output=[];
+        for i in range(0,max(result)+1):
+            temp_list=[];
+            for j in range(0,len(result)):
+                if result[j]!=i:
+                    temp_list+=[0]
+                else:
+                    temp_list+=[1]
+            output.append(temp_list)
+        return output
+
+
--- webapp/models.py
+++ webapp/models.py
@@ -1,66 +1,44 @@
-from flask import Blueprint, render_template, session, redirect, url_for, \
-     request, flash, g, jsonify, abort
 import os
+from flask import current_app as app
 
-mod = Blueprint('models', __name__)
+from engine.graph import DGraph
+
+EXTENSION = '.dot'
 
 def _remove_ext(filename):
     "filename.ext -> filename"
     # rfind returns -1 on not found, so we'll get the full filename
-    return filename[filename.rfind('.') + 1:]
-
-def _filename_to_model_name(filename):
-    # basename does  /path/to/file -> file
-    return os.path.basename(_remove_ext(filename))
+    return filename[:filename.rfind('.')]
 
 def _model_name_to_filename(model_name):
-    return "{}.{}".format(model_name, EXTENSION)
+    return "{}{}".format(model_name, EXTENSION)
 
 class Models:
+    """
+    Access to saved models (raw graphs).
+    No caching.
+    """
     def __init__(self, base_dir):
         self.base_dir = base_dir
 
     def list(self):
-        return [_filename_to_model_name(filename) for filename in os.listdir(self.base_dir)]
+        "Return list of model ids"
+        # all valid files transformed to model ids
+        model_ids = [self.filename_to_model_name(filename) for filename in os.listdir(self.base_dir)
+                            if self.allowed_filename(filename)]
+        return model_ids
 
+    def open(self, model_id):
+        model_path = os.path.join(self.base_dir, _model_name_to_filename(model_id))
+        return DGraph.read_dot(model_path)
 
-EXTENSION = 'dot'
+    @staticmethod
+    def allowed_filename(filename):
+        "True if filename ends with '.dot'"
+        return filename.endswith(EXTENSION)
 
+    @staticmethod
+    def filename_to_model_name(filename):
+        # basename does  /path/to/file -> file
+        return os.path.basename(_remove_ext(filename))
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1]==EXTENSION
-
-@mod.route('/models', methods=['GET', 'POST'])
-def models():
-    if request.method == 'GET':
-        return jsonify(models=Models.list())
-
-    # method == POST, handle upload
-    if 'file' not in request.files:
-        raise Exception("TODO: missing file in post handling")
-        
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit a empty part without filename
-    if file.filename:
-        raise Exception("TODO: no file in post handling")
-
-    if not file:
-        raise Exception("TODO: no file case 3?!?")
-
-    if not allowed_file(file.filename):
-        raise Exception("Allowed file extension '.{}'".format(EXTENSION))
-
-    # secure filename to stop directory traversals, etc.
-    filename = secure_filename(file.filename)
-
-    # write the file, finally 
-    file.save(os.path.join(mod.config['MODELS_PATH'], filename))
-
-    #TODO: return list of models anyway
-    return redirect(url_for('model', model_name=filename))
-
-@mod.route('/model/<model_name>')
-def model(model_name):
-    return send_from_directory(mod.config['MODELS_PATH'],
-                               _model_name_to_filename(model_name))
