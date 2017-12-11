dee25a861cc2d9947ba49b0f44e9a32aba1fcd13
Roee Shenberg <shenberg@gmail.com>
2017-12-11 21:34:52 +0200

    test partition

---
+++ engine/test_partition.py
@@ -0,0 +1,18 @@
+from engine.baisc_entities.graph import DGraph
+from engine.clustering import SpectralCluster
+from engine.stopping_criteria.stopCriteria import SizeCriteria
+from engine.baisc_entities.dendrogram import Node,Dendrogram
+
+
+def test_partition():
+    den = None
+    g = DGraph.read_dot("./dot/weighted_g2.dot")
+    print("testing partition on g2.dot for threshold=4:")
+    den = partition(g,g.nodes(), SpectralCluster.SpectralCluster, SizeCriteria(4))
+
+    assert len(den.node_list) == 3
+    
+    print("testing partition on g2.dot for threshold=2:")
+    den = partition(g,g.nodes(), SpectralCluster.SpectralCluster, SizeCriteria(2),dendrogram=None)
+    assert len(den.node) == 7
+    
