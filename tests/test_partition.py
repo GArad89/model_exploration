441bb82a0f4d10f3c19f22f3f78b43643c0ea68e
Roee Shenberg <shenberg@gmail.com>
2017-12-26 18:07:56 +0200

    fixed partition test

--- tests/test_partition.py
+++ tests/test_partition.py
@@ -2,6 +2,7 @@ from engine.baisc_entities.graph import DGraph
 from engine.clustering import SpectralCluster
 from engine.stopping_criteria.stopCriteria import SizeCriteria
 from engine.baisc_entities.dendrogram import Node,Dendrogram
+from engine.main.partitionLoop import partition
 
 
 def test_partition():
@@ -14,5 +15,5 @@ def test_partition():
     
     print("testing partition on g2.dot for threshold=2:")
     den = partition(g,g.nodes(), SpectralCluster.SpectralCluster, SizeCriteria(2),dendrogram=None)
-    assert len(den.node) == 7
+    assert len(den.node_list) == 7
     
