4131e894b091957c3eeacdc3cfafe425ae2e73aa
Roee Shenberg <shenberg@gmail.com>
2017-12-26 17:58:06 +0200

    more minor test stuff

--- tests/test_kernighan_lin.py
+++ tests/test_kernighan_lin.py
@@ -1,19 +1,17 @@
 import sys
 sys.path.append('..')
-
+from networkx.classes import graph
 from engine.clustering.minimum_cut import MinimumCut
 from engine.clustering.KernighanLinCluster import KernighanLinCluster
 from engine.baisc_entities.graph import DGraph
-from main.partitionLoop import partition
-from networkx.classes import graph
-from stopping_criteria.stopCriteria import SizeCriteria
-def test():
+from engine.main.partitionLoop import partition
+from engine.stopping_criteria.stopCriteria import SizeCriteria
+
+def test_kerningham_lin():
     den = None
-    g = DGraph.read_dot("../engine/dot/cvs.net.mutated.dot")
+    g = DGraph.read_dot("engine/dot/cvs.net.mutated.dot")
     mc = MinimumCut()
     res = mc.cluster(g)
 
     partition(g, clustering_algo = KernighanLinCluster, stopCri = SizeCriteria(3))
 
-
-test()
