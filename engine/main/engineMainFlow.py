d340064335571147e7d9c81b592152115c36bb94
Roee Shenberg <shenberg@alooma.io>
2018-01-02 14:18:48 +0200

    use importlib for importing

--- engine/main/engineMainFlow.py
+++ engine/main/engineMainFlow.py
@@ -1,24 +1,21 @@
-from ..baisc_entities.graph import *
-from ..baisc_entities.dendrogram import *
-from ..clustering import *
-from .partitionLoop import *
-from ..stopping_criteria.stopCriteria import *
-from ..labeling import TfIdfLabeler
-from ..utils.jsonWorker import *
+from engine.baisc_entities.graph import *
+from engine.baisc_entities.dendrogram import *
+from engine.clustering import *
+from engine.stopping_criteria.stopCriteria import *
+from engine.labeling import TfIdfLabeler
+from engine.utils.jsonWorker import *
+from .partitionLoop import partition
 
+import importlib
 
 def run_algo(graph,algo_name,params,stopCriteria):
     #parsing from string to algo object
-    module = __import__('engine.clustering.'+algo_name)
-    module=getattr(module, 'clustering')
-    module=getattr(module, algo_name)
+    module = importlib.import_module('engine.clustering.'+algo_name)
     algo_class = getattr(module, algo_name) #we want the algorithm class and not an initialized algorithm
     #algo = class_() #now we have an object
     
     #parsing from string to stop criteria object
-    module = __import__('engine.stopping_criteria.stopCriteria')
-    module=getattr(module, 'stopping_criteria')
-    module=getattr(module, 'stopCriteria')
+    module = importlib.import_module('engine.stopping_criteria.stopCriteria')
     class_ = getattr(module, stopCriteria) 
     stopCri = class_(2) #now we have an object #the 20 is the threshold TODO change later
     dendro=partition(graph,graph.nodes(), algo_class, stopCri)
