b4a73fc894291b3c6fa0057a15502d6b58cbada2
yaron <yaron314@gmail.com>
2018-01-12 14:13:07 +0200

    added PathLabeler to __init__.py

--- engine/labeling/__init__.py
+++ engine/labeling/__init__.py
@@ -3,7 +3,7 @@ from . import PrefixLabeler, TfIdfLabeler, PageRankLabeler, PathLabeler
 
 def get_methods():
     "return list of all supported labeling method classes"
-    return [PrefixLabeler.PrefixLabeler, TfIdfLabeler.TfIdfLabeler, PageRankLabeler.PageRankLabeler]
+    return [PrefixLabeler.PrefixLabeler, TfIdfLabeler.TfIdfLabeler, PageRankLabeler.PageRankLabeler, PathLabeler.PathLabeler]
 
 def get_labeling_method(name):
     "return class for specific labeling method"
