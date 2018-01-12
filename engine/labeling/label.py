383ed70f72538f28b74a1887d7ce322f9acd3f21
yaron <yaron314@gmail.com>
2018-01-12 14:47:09 +0200

    removed prints

--- engine/labeling/label.py
+++ engine/labeling/label.py
@@ -36,22 +36,15 @@ class GraphLabeler(ABC):
             labeling_on_type.EDGES_AND_NODES: itertools.chain(subgraph.edges.items(), subgraph.nodes.items())
         }.get(self.source, [])
 
-    def shortenlabel(self,label):
-        print("MAMAMA")
-        print(label)
+    def shortenlabel(self,label, max_lables=5):
         lables_list = re.split(',|_|-|,\n|\n',label)
-        print(lables_list)
-        max_lables = 5
         l = len(lables_list)
         if  l > max_lables:
             newlist = [lables_list[0]]
-            print("HEREq")
             print(newlist)
             for i in range(1,max_lables-1):
                 newlist += [lables_list[(l//(max_lables-1))*i]]
             newlist += [lables_list[-1]]
-            print("HERE")
-            print(newlist)
             return ','.join(newlist)
         else:
             return label
