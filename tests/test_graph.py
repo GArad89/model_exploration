1b0028aeef2bc8b73489dabcdfe0381e17cb2663
nimrodbu7 <nimibu76?>
2018-01-16 17:47:52 +0200

    nothing ness

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,6 +1,6 @@
 import unittest
 from engine.baisc_entities.graph import *
-
+from utils import *
 
 class Test_DGraph(unittest.TestCase):
 
@@ -32,24 +32,24 @@ class Test_DGraph(unittest.TestCase):
 class Test_GraphImporter(unittest.TestCase):
 
     def test_import_example(self):
-        self.assertIsNotNone(DGraph.read_dot(join(project_root(), "engine/dot/example.dot")),"import of example.dot failed")
-        g=DGraph.read_dot(join(project_root(), "engine/dot/example.dot"))
+        self.assertIsNotNone(DGraph.read_dot("./engine/dot/example.dot"),"import of example.dot failed")
+        g=DGraph.read_dot("./engine/dot/example.dot")
         self.assertEqual(len(g.nodes()),24, "wrong number of nodes after import")
         
     def test_import_zip(self):
-        self.assertIsNotNone(DGraph.read_dot(join(project_root(), "engine/dot/zip.dot")),"import of zip.dot failed")
-        g=DGraph.read_dot(join(project_root(), "engine/dot/zip.dot"))
+        self.assertIsNotNone(DGraph.read_dot("./engine/dot/zip.dot"),"import of zip.dot failed")
+        g=DGraph.read_dot("./engine/dot/zip.dot")
         self.assertEqual(len(g.nodes()),5, "wrong number of nodes after import")
         self.assertEqual(len(g.edges()),9, "wrong number of edges after import")
 
 class Test_GraphExporter(unittest.TestCase):
 
     def test_import_and_export(self):
-        self.assertIsNotNone(DGraph.read_dot(join(project_root(), "engine/dot/example.dot")), "import of example.dot failed")
-        g=DGraph.read_dot(join(project_root(), "engine/dot/example.dot"))
-        g.write_dot(join(project_root(), "tests/dot_export/example.dot"))
-        self.assertIsNotNone(DGraph.read_dot(join(project_root(), "tests/dot_export/example.dot")), "import of exported example.dot failed")
-        g=DGraph.read_dot(join(project_root(), "tests/dot_export/example.dot"))
+        self.assertIsNotNone(DGraph.read_dot("./engine/dot/example.dot"),"import of example.dot failed")
+        g=DGraph.read_dot("./engine/dot/example.dot")
+        g.write_dot("./tests/dot_export/example.dot")
+        self.assertIsNotNone(DGraph.read_dot("./tests/dot_export/example.dot"),"import of exported example.dot failed")
+        g=DGraph.read_dot("./tests/dot_export/example.dot")
         self.assertEqual(len(g.nodes()),24, "wrong number of nodes after export")
         
     def test_builtgraph_export(self):
@@ -60,9 +60,9 @@ class Test_GraphExporter(unittest.TestCase):
         g.add_node('4')
         g.add_edge('1','2',weight=2)
         g.add_edge('3','2',weight=2)
-        g.write_dot(join(project_root(), "tests/dot_export/g.dot"))
-        self.assertIsNotNone(DGraph.read_dot(join(project_root(), "tests/dot_export/g.dot")), "import of exported g.dot failed")
-        g=DGraph.read_dot(join(project_root(), "tests/dot_export/g.dot"))
+        g.write_dot("./tests/dot_export/g.dot")
+        self.assertIsNotNone(DGraph.read_dot("./tests/dot_export/g.dot"),"import of exported g.dot failed")
+        g=DGraph.read_dot("./tests/dot_export/g.dot")
         self.assertEqual(len(g.nodes()),4, "wrong number of nodes after export")
         self.assertEqual(len(g.edges()),2, "wrong number of edges after export")
 
