20e8f8a13385dd6007e1c5e391242777c2053878
Roee Shenberg <shenberg@gmail.com>
2018-01-16 18:54:49 +0200

    exception instead of silent fix

--- engine/clustering/SpectralCluster.py
+++ engine/clustering/SpectralCluster.py
@@ -32,14 +32,14 @@ class SpectralCluster(Cluster):
         }
         return schema, form
 
-
         
     def cluster(self, dgraph):
         
         #number of clusters can't be bigger than the number of nodes
-        if(self.n>=len(dgraph.nodes())): n_clusters=len(dgraph.nodes())-1
-        else: n_clusters=self.n
-  
+        if(self.n>=len(dgraph.nodes())): 
+            raise ValueError("Not enough nodes in graph, expected at least {}".format(self.n))
+        else: 
+            n_clusters=self.n  
 
         #adjacency matrix
         adj_mat = dgraph.adjacency_matrix()
