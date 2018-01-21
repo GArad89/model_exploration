ecda31b1dd6afbf0f4c511f0d2918cdf715b93bc
Guy arad <garad89@gmail.com>
2018-01-21 10:48:35 +0200

    RandomWalk labeler working for edges and both

--- engine/labeling/RandomWalkLabeler.py
+++ engine/labeling/RandomWalkLabeler.py
@@ -12,19 +12,20 @@ class RandomWalkLabeler(GraphLabeler):
         return subset_sorted[:k]
 
     def n_important_edges(self,subset_edges, n):
-        print("chk3")
         edges_ranks = {}
         for edge in subset_edges:
+            
             out_node = edge[0]
             out_node_rank = self.ranks[out_node]
             edges_ranks[(edge[0], edge[1])] = out_node_rank / len(self.graph.dgraph.out_edges())
-
-        log.debug(edges_ranks)
+        #log.debug(edges_ranks)  #crashes the webapp for some reason (http error 500. no other feedback)
 
         k = min(n, len(subset_edges))
         subset_sorted = sorted(subset_edges, key=lambda edge: edges_ranks[(edge[0], edge[1])], reverse=True)
+        
         return subset_sorted[:k]
-    
+        
+        
     def __init__(self, graph, dendrogram, source):
         super().__init__(graph, dendrogram, source)
         self.google_mat = nx.google_matrix(self.graph.dgraph)
@@ -50,15 +51,22 @@ class RandomWalkLabeler(GraphLabeler):
         X=temp
         self.ranks = dict(zip(nodes,X))
         chosen_labels=[]
-        for super_node in self.dendrogram.nodes():
+        for super_node in self.dendrogram.nodes()[1:]:
             if self.source == labeling_on_type.NODES:
                 chosen_nodes = self.n_important_nodes(super_node.subset, label_size)
                 chosen_labels = [self.graph.node_attr(node, 'label') for node in chosen_nodes]
-            elif (self.source == labeling_on_type.EDGES)or(self.source == labeling_on_type.EDGES_AND_NODES):
-                print("chk3")
+            elif (self.source == labeling_on_type.EDGES):
                 chosen_edges = self.n_important_edges(super_node.projected_graph.edges(), label_size)
-                print("chk3")
                 chosen_labels = [self.graph.dgraph.edges[(edge[0], edge[1])].get('label','') for edge in chosen_edges]
+            else:  #both edges and nodes
+                chosen_nodes = self.n_important_nodes(super_node.subset, label_size)
+                chosen_labels_nodes = [self.graph.node_attr(node, 'label') for node in chosen_nodes]
+                chosen_edges = self.n_important_edges(super_node.projected_graph.edges(), label_size)
+                chosen_labels_edges = [self.graph.dgraph.edges[(edge[0], edge[1])].get('label','') for edge in chosen_edges]
+                for i in range(int(len(chosen_labels_nodes)/2)): #since the edge and node ranks arent normalized, they can't be compared. we "mix" the labels as such: node,edge,node...
+                    chosen_labels+=[chosen_labels_nodes[i]]
+                    chosen_labels+=[chosen_labels_edges[i]]
+            
             chosen_labels = [l for l in chosen_labels if l]
             # super_node.label = super().shorten_label(','.join(chosen_nodes_labels))
             super_node.label = super().shorten_label(chosen_labels)
