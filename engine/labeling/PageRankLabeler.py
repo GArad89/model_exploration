3d8771eb6719cb23a94412ad336658c0f5eb55e5
Guy arad <garad89@gmail.com>
2018-01-21 11:32:06 +0200

    pagerank actually works for graphs with weight now. also added "both"
    option

--- engine/labeling/PageRankLabeler.py
+++ engine/labeling/PageRankLabeler.py
@@ -11,9 +11,7 @@ class PageRankLabeler(GraphLabeler):
         super().__init__(graph, dendrogram, source)
 
     def label(self, label_size=3):
-
-        ranks = nx.pagerank(self.graph.dgraph)
-
+        ranks = nx.pagerank_numpy(self.graph.dgraph)
         def n_important_nodes(subset, n):
             nonlocal ranks
             k = min(n, len(subset))
@@ -33,15 +31,22 @@ class PageRankLabeler(GraphLabeler):
             k = min(n, len(subset_edges))
             subset_sorted = sorted(subset_edges, key=lambda edge: edges_ranks[(edge[0], edge[1])], reverse=True)
             return subset_sorted[:k]
-
+        chosen_labels=[]
         for super_node in self.dendrogram.nodes()[1:]:
             if self.source == labeling_on_type.NODES:
                 chosen_nodes = n_important_nodes(super_node.subset, label_size)
                 chosen_labels = [self.graph.node_attr(node, 'label') for node in chosen_nodes]
-            elif self.source == labeling_on_type.EDGES \
-                or self.source == labeling_on_type.EDGES_AND_NODES:                 # TODO: something smarter on edges+nodes mode
+            elif self.source == labeling_on_type.EDGES:
                 chosen_edges = n_important_edges(super_node.projected_graph.edges(), label_size)
                 chosen_labels = [self.graph.dgraph.edges[(edge[0], edge[1])].get('label','') for edge in chosen_edges]
+            else:  #both edges and nodes
+                chosen_nodes = n_important_nodes(super_node.subset, label_size)
+                chosen_labels_nodes = [self.graph.node_attr(node, 'label') for node in chosen_nodes]
+                chosen_edges = n_important_edges(super_node.projected_graph.edges(), label_size)
+                chosen_labels_edges = [self.graph.dgraph.edges[(edge[0], edge[1])].get('label','') for edge in chosen_edges]
+                for i in range(int(len(chosen_labels_nodes)/2)): #since the edge and node ranks arent normalized, they can't be compared. we "mix" the labels as such: node,edge,node...
+                    chosen_labels+=[chosen_labels_nodes[i]]
+                    chosen_labels+=[chosen_labels_edges[i]]
             chosen_labels = [l for l in chosen_labels if l]
             log.debug(chosen_labels)
             # super_node.label = super().shorten_label(','.join(chosen_labels))
